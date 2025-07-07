6e05e11fe979949835d5ff452dde4d01270c026e
Anshfinds <retired1234321@gmail.com>
2025-07-07 16:00:22 +0000

    operation

--- basics/03_ConversionOperation.js
+++ basics/03_ConversionOperation.js
@@ -1,13 +1,24 @@
 let score = 3
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 let stringnumber = String(score)
-console.log(score);
-console.log(typeof stringnumber);
+// console.log(score);
+// console.log(typeof stringnumber);
 
+// *************operation******************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "hello"
+let str2 = "Ansh"
+
+let str3 = str1 +str2
+console.log(str3)
